d3a42bec1cc3b341c9b7d8dbbaa2a57df77643bb
Dede Bakaswa <>
2016-02-11 11:33:02 +0100

    Dede

--- src/main/java/com/kin/sample2/Calcul.java
+++ src/main/java/com/kin/sample2/Calcul.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-//Ce programme est modifié pour le test
+//Ce programme est modifié pour le test.
 package com.kin.sample2;
 
 /**
